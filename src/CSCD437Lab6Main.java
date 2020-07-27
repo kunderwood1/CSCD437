63431081014b30e43809d0d47f3d0cbca10df4ac
jgoodnight72 <jgoodnight@eagles.ewu.edu>
2020-07-27 13:22:39 -0700

    Stubbing out the stubbies

--- src/CSCD437Lab6Main.java
+++ src/CSCD437Lab6Main.java
@@ -1,16 +1,14 @@
 import java.io.File;
 import java.util.Scanner;
 
-
 public class CSCD437Lab6Main {
 
     /*
     *
-    *Author: Kevin Underwood
+    *Author: Kevin Underwood, Jennifer Goodnight, Zachary Stuefen
     *Version 0.1
     *
     */
-
 
     public static void main(String[] args) {
 
@@ -21,30 +19,42 @@ public class CSCD437Lab6Main {
         PasswordInput(kb);
     }
 
-
-
-    private static void FileInput(Scanner kb) {
-        System.out.println("Please enter the name of your input file: ");
-        System.out.println("Please enter the name of your output file: ");
-    }
-
-
+    //#1
     private static void nameInput(Scanner kb) {
         System.out.println("Please enter your first name: ");
         System.out.println("Please enter your Last name: ");
     }
-    private static void IntInput(Scanner kb) {
+
+    //#2
+    private static void intInput(Scanner kb) {
         System.out.println("Please enter an integer value: ");
         System.out.println("Please enter another integer value: ");
+    }
+
+    //#3
+    private static void fileNameInput(Scanner kb) {
+        System.out.println("Please enter the name of your input file: ");
 
     }
 
-    private static void PasswordInput(Scanner kb) {
+    //#4
+    private static void fileNameOutput(Scanner kb) {
+        System.out.println("Please enter the name of your output file: ");
+    }
+
+    //#5
+    private static void passwordInput(Scanner kb) {
         File passwordFile = new File("Password.txt");
         System.out.println("Please enter your password: ");
-        System.out.println("Please enter your password: ");
+        System.out.println("Please re-enter your password: ");
     }
-    private static String Validate(String input) {
+
+    //#7
+    private static void openOutputFile(Scanner kb) {
+
+    }
+
+    private static String validate(String input) {
         return "";
     }
 }
