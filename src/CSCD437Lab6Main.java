e9c2ef7b9eae332ef9a8df0006fc797f891def56
= <=>
2020-07-27 15:18:37 -0700

    Worked on fileValidate, fileNameInput, and fileNameOutput. Created text.txt for testing input.

--- src/CSCD437Lab6Main.java
+++ src/CSCD437Lab6Main.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CSCD437Lab6Main {
 
@@ -33,26 +36,47 @@ public class CSCD437Lab6Main {
         System.out.println("Please enter another integer value: ");
     }
 
-    //#3
+    //#3 Made text.txt for testing.
     private static void fileNameInput(Scanner kb) {
+        File inpFile = null;
         String fileNameIn = null;
-        File fileIn = null;
         do {
             System.out.println("Please enter the name of your input file: ");
             fileNameIn = kb.nextLine();
-            fileIn = new File("fileNameIn");
-        } while(fileNameIn == null || !fileIn.exists());
+            if(fileValidate(fileNameIn)) { // Checks if file name is valid
+                inpFile = new File(fileNameIn);
+            }
+            if (inpFile == null || !inpFile.exists()) {
+                System.out.println("Please try again.");
+            }
+        } while(inpFile == null || !inpFile.exists());
     }
 
     //#4
     private static void fileNameOutput(Scanner kb) {
+        File outFile = null;
         String fileNameOut = null;
-        File fileOut = null;
         do {
             System.out.println("Please enter the name of your output file: ");
             fileNameOut = kb.nextLine();
-            fileOut = new File("fileNameOut");
-        } while(fileNameOut == null || !fileOut.exists());
+
+            if(fileValidate(fileNameOut)) { // Checks if file name is valid
+                File f = new File(fileNameOut);
+
+                if(!f.exists()) { // Checks to make sure the file exists
+                    try { // If the file name works, but the file does not exist, will try to create the file to use.
+                        f.createNewFile();
+                        outFile = f;
+                    } catch (IOException e) {
+                        System.out.println("Unable to create file, please try again or enter an already existing file.");
+                    }
+                } else { // File already exists
+                    outFile = f;
+                }
+            } else { // File is not validated.
+                System.out.println("Please try again.");
+            }
+        } while(outFile == null);
     }
 
     //#5
@@ -69,5 +93,17 @@ public class CSCD437Lab6Main {
 
     private static String validate(String input) {
         return "";
+    }
+
+    private static boolean fileValidate(String input) {
+        if(input == null || input.isEmpty() || input.length() >= 233) // Maximum characters windows allows in a file name is 233, this includes the extension
+            return false;
+
+        String filePattern = "^[^\\\\|/|*|:|?|\"|<|>|\\|]*$";
+        Pattern pattern = Pattern.compile(filePattern);
+        Matcher match = pattern.matcher(input);
+        boolean res = match.matches();
+
+        return res;
     }
 }
