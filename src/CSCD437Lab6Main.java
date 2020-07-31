88a7ee0d3c0df47c063dd92ce5520633a6e6f4b3
= <=>
2020-07-30 17:08:29 -0700

    Worked on fileValidate, fileNameInput, and fileNameOutput. Created text.txt for testing input.

--- src/CSCD437Lab6Main.java
+++ src/CSCD437Lab6Main.java
@@ -1,5 +1,11 @@
-import java.io.File;
-import java.io.IOException;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.io.*;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,21 +17,27 @@ public class CSCD437Lab6Main {
      *Version 0.2.6
      */
 
+    private static String firstName;
+    private static String lastName;
+    private static int int1;
+    private static int int2;
+    private static File inpFile;
+    private static File outFile;
+
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         nameInput(kb);
         intInput(kb);
         fileNameInput(kb);
         fileNameOutput(kb);
-        passwordInput(kb);
+        //passwordInput(kb);
         openOutputFile(kb);
-        String str = validate("");
     }
 
     //#1
     private static void nameInput(Scanner kb) {
-        String firstName = nameHelper(kb,"first");
-        String lastName = nameHelper(kb,"Last");
+        firstName = nameHelper(kb,"first");
+        lastName = nameHelper(kb,"Last");
         System.out.println("First name : "+firstName);
         System.out.println("Last name : "+lastName);
     }
@@ -42,8 +54,8 @@ public class CSCD437Lab6Main {
 
     //#2
     private static void intInput(Scanner kb) {
-        int int1 = intHelper(kb, "first");
-        int int2= intHelper(kb, "second");;
+        int1 = intHelper(kb, "first");
+        int2= intHelper(kb, "second");;
         System.out.println("Integer one : "+int1);
         System.out.println("Integer two : "+int2);
         kb.nextLine();
@@ -59,9 +71,10 @@ public class CSCD437Lab6Main {
         return kb.nextInt();
     }
 
+    //#3
     private static void fileNameInput(Scanner kb) {
         String fileNameIn = null;
-        File fileIn = null;
+        inpFile = null;
         do {
             System.out.println("Please enter the name of your input file: ");
             fileNameIn = kb.nextLine();
@@ -77,7 +90,7 @@ public class CSCD437Lab6Main {
     //#4
     private static void fileNameOutput(Scanner kb) {
         String fileNameOut = null;
-        File fileOut = null;
+        outFile = null;
         do {
             System.out.println("Please enter the name of your output file: ");
             fileNameOut = kb.nextLine();
@@ -263,7 +276,21 @@ public class CSCD437Lab6Main {
 
     //#7
     private static void openOutputFile(Scanner kb) {
-
+        try {
+            FileWriter fOut = new FileWriter(outFile);
+            fOut.write(firstName + " ");
+            fOut.write(lastName + "\n");
+            fOut.write(StrictMath.addExact(int1, ((long)int2)) + "\n");
+            fOut.write(StrictMath.multiplyFull(int1, int2) + "\n");
+            Scanner inf = new Scanner(inpFile);
+            while(inf.hasNext()) {
+                fOut.write(inf.nextLine() + "\n");
+            }
+            fOut.close();
+        } catch (IOException e) {
+            System.out.println("failed?");
+            e.printStackTrace();
+        }
     }
 
     private static boolean fileValidate(String input) {
